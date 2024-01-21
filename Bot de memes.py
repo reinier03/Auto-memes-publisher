5debfcfe21e9b4e61ebc4e170c546acec91e7171
Reima <reiniermayea@gmail.com>
2024-01-21 04:44:13 -0500

    changed

--- Bot de memes.py
+++ Bot de memes.py
@@ -2,13 +2,13 @@ import os
 import requests
 from bs4 import BeautifulSoup as bs
 from time import sleep
-import easy_bot_reima
+import discord
 import threading
 
 
 
-intentos = easy_bot_reima.discord.Intents.all()
-client = easy_bot_reima.EasyBot(intents=intentos)
+intentos = discord.Intents.all()
+client = discord.Client(intents=intentos)
 
 
 user={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0"}
