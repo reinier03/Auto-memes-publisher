f1b838473c3e8e61bba83b83a553dc9f89a2c108
Reima <reiniermayea@gmail.com>
2024-01-21 03:56:50 -0500

    first commit from this branch

--- Bot de memes.py
+++ Bot de memes.py
@@ -1,12 +1,17 @@
 import os
 import requests
 from bs4 import BeautifulSoup as bs
-import telebot
 from time import sleep
+import easy_bot_reima
+import threading
+
+
+
+intentos = easy_bot_reima.discord.Intents.all()
+client = easy_bot_reima.EasyBot(intents=intentos)
 
 
 user={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0"}
-bot=telebot.TeleBot(os.environ["token"])
 diccionario={}
 
 
@@ -17,7 +22,7 @@ def obtener_memes():
     diccionario.clear()
     while not len(diccionario)>=48:
         contador+=1
-        res=requests.get(f"https://es.memedroid.com/memes/random?page={contador}", headers=user)
+        res=requests.get(f"https://www.memedroid.com/memes/random?page={contador}", headers=user)
         soup=bs(res.text, features="html.parser")
         articulos=soup.find_all("article", class_="gallery-item")
         for e, i in enumerate(articulos, start=len(diccionario)+1):
@@ -39,7 +44,7 @@ def obtener_memes():
 
 
 def publicar(diccionario, user):
-    target=-1001161864648 #reemplazar este valor con el chat id del destino
+    canal=client.get_channel(1189687855774191687) #reemplazar este valor con el chat id del destino
     for e, i in enumerate(diccionario, start=1):
         res=requests.get(diccionario[e][0], headers=user)
         
@@ -47,10 +52,8 @@ def publicar(diccionario, user):
             archivo_escritura.write(res.content)
             
         archivo_lectura=open(f"{os.path.basename(diccionario[e][0])}", "rb")
-        if os.path.basename(diccionario[e][0]).split('.')[-1] == "jpeg":
-            bot.send_photo(target, photo=archivo_lectura, caption=diccionario[e][1])
-        else:
-            bot.send_document(target, document=archivo_lectura, caption=diccionario[e][1])
+        archivo=easy_bot_reima.discord.File(archivo_lectura)
+        canal.send(f"{diccionario[e][1]}",file=archivo) 
                 
         archivo_lectura.close()
         os.remove(os.path.basename(diccionario[e][0]))
@@ -58,12 +61,14 @@ def publicar(diccionario, user):
     return
             
                 
-                
-                
-obtener_memes()
+@client.event
+async def on_ready():
+    while True:
+        await obtener_memes()
 
-bot.infinity_polling()
-    
+
+
+client.run(os.environ["token"])
     
     
 
